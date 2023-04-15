caa2874189930a334f451da6fb75174ee50a1024
oderathegreat <oderathegreat@gmail.com>
2023-04-15 11:15:26 +0300

    fix python code plus chatgpt integrations

--- django_botgpt/views.py
+++ django_botgpt/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.contrib import messages
 import openai
 
 
 def home(request):
-    # api-key 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
     lang_list = ['C++', 'Go', 'Java', 'JavaScript', 'Kotlin', 'PHP', 'Ruby', 'Rust', 'Swift', 'TypeScript']
     if request.method == "POST":
         code = request.POST['code']
@@ -14,25 +13,24 @@ def home(request):
             messages.success(request, "You forgot to pick programming langage")
             return render(request, "index.html", {'lang_list': lang_list, 'code': code, 'lang': lang})
 
-        openai.api_key = "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"
+        openai.api_key = "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"
         openai.Model.list()
 
-        #make a request to api
         try:
-
             response = openai.Completion.create(
                 model="text-davinci-003",
                 prompt=f"Respond Only With Code. Fix this {lang} code: {code}",
                 temperature=0,
-                max_tokens = 1000,
-                top_p = 1.0,
-                frequncy_penalty = 0.0,
-                presence_panalty = 0.0
-
+                max_tokens=1000,
+                top_p=1.0,
+                frequncy_penalty=0.0,
+                presence_panalty=0.0
             )
+
+            return render(request, "index.html", {'lang_list': lang_list, 'response': response, 'lang': lang})
+
         except Exception as e:
+            messages.error(request, str(e))
+            return render(request, "index.html", {'lang_list': lang_list, 'code': code, 'lang': lang})
 
-
-
-    # print(sorted(lang_list))
     return render(request, "index.html", {'lang_list': lang_list})
