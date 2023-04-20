190bfb13ca509618d30a346a825ead2bd0c90e12
oderathegreat <oderathegreat@gmail.com>
2023-04-20 16:51:55 +0300

    register user form logic

--- django_botgpt/urls.py
+++ django_botgpt/urls.py
@@ -24,6 +24,4 @@ urlpatterns = [
     path('login', views.loginUser, name='login'),
     path('logout', views.logoutUser, name='logout'),
     path('register', views.registerUser, name='register')
-
-
 ]
--- django_botgpt/views.py
+++ django_botgpt/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 
 import openai
 
@@ -97,3 +98,20 @@ def logoutUser(request):
     messages.success(request, "You have been logged Out")
     return redirect('/')
 
+
+def registerUser(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username,password=password)
+            login(request, user)
+            messages.success(request, "You have been registered successfully")
+            return redirect("/")
+
+    else:
+        form = SignUpForm
+    return render(request, "register.html", {"form" : form})
